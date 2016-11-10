b1a3f79085bb97c85cb4ca50b7e1ffa25389b156
codeworm96 <codeworm96@outlook.com>
2016-11-10 18:47:14 +0800

    Add cmdline arguments parsing

---
+++ README.md
@@ -0,0 +1,6 @@
+# dy-weekly-generator
+
+## Roadmap
+[X] Parse cmdline arguments
+[ ] Fetch comments
+[ ] Render weekly to file
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::env;
+
+#[derive(Debug)]
+struct Config {
+    output: String,
+    repo: String,
+    issue: String,
+    key: String,
+}
+
+enum WeeklyErr {
+    ConfigErr,
+}
+
+fn parse_args() -> Result<Config, WeeklyErr> {
+    let mut args = env::args();
+    args.next();
+    let output = try!(args.next().ok_or(WeeklyErr::ConfigErr));
+    let repo = try!(args.next().ok_or(WeeklyErr::ConfigErr));
+    let issue = try!(args.next().ok_or(WeeklyErr::ConfigErr));
+    let key = try!(args.next().ok_or(WeeklyErr::ConfigErr));
+    match args.next() {
+        Some(_) => Err(WeeklyErr::ConfigErr),
+        None => Ok(Config {
+            output: output,
+            repo: repo,
+            issue: issue,
+            key: key
+        }),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = match parse_args() {
+        Ok(c) => c,
+        Err(_) => {
+            println!("Usage: dy-weekly-generator <output> <repo> <issue> <key>");
+            return;
+        }
+    };
+    println!("{:?}", config);
 }
