c41af383388bdf23253277ba3cdf56b86db4ef6b
Zhang Yuning <codeworm96@outlook.com>
2018-05-09 13:21:12 +0800

    Add paging handling (#11)
    * Add paging handling
    
    * format
    
    * Refactor & make it builds under stable

--- src/github.rs
+++ src/github.rs
@@ -1,23 +1,64 @@
 use json;
 use reqwest;
-use reqwest::header::{qitem, Accept, Authorization, Headers, UserAgent};
+use reqwest::header::{qitem, Accept, Authorization, Headers, Link, RelationType, UserAgent};
 use reqwest::mime::Mime;
 use reqwest::Client;
 
 use std::io::Read;
+use std::mem;
 
 use error::Error;
 
 const API_ROOT: &'static str = "https://api.github.com";
 
-pub fn fetch<'a>(
-    repo: &str,
-    issue: &str,
-    key: Option<&str>,
-) -> Result<impl Iterator<Item = String>, Error> {
-    let client = Client::new();
-    let url = format!("{}/repos/{}/issues/{}/comments", API_ROOT, repo, issue);
+pub struct Comments<'a> {
+    client: Client,
+    key: Option<&'a str>,
+    comments: Box<Iterator<Item = String>>,
+    next: Option<String>,
+}
 
+impl<'a> Iterator for Comments<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        loop {
+            if let Some(s) = self.comments.next() {
+                return Some(s);
+            } else {
+                if let Some(url) = mem::replace(&mut self.next, None) {
+                    if let Ok(page) = fetch_page(&self.client, &url, self.key) {
+                        self.comments = page.comments;
+                        self.next = page.next;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+        }
+    }
+}
+
+struct Page {
+    comments: Box<Iterator<Item = String>>,
+    next: Option<String>,
+}
+
+fn next_link(headers: &Headers) -> Option<&str> {
+    let link: &Link = headers.get()?;
+    for v in link.values() {
+        if let Some(rel) = v.rel() {
+            if rel.contains(&RelationType::Next) {
+                return Some(v.link());
+            }
+        }
+    }
+    None
+}
+
+fn fetch_page(client: &Client, url: &str, key: Option<&str>) -> Result<Page, Error> {
     let mut headers = Headers::new();
     let accept_mime: Mime = "application/vnd.github.v3+json".parse().unwrap();
     headers.set(Accept(vec![qitem(accept_mime)]));
@@ -27,22 +68,38 @@ pub fn fetch<'a>(
         None => {}
     }
 
-    let mut res = client.get(&url).headers(headers).send()?;
+    let mut res = client.get(url).headers(headers).send()?;
 
     if res.status() != reqwest::StatusCode::Ok {
         Err(Error::FetchErr(res))
     } else {
         let mut content = String::new();
         res.read_to_string(&mut content)?;
-        let comments = json::parse(&content)?;
-        match comments {
-            json::JsonValue::Array(cs) => {
-                Ok(cs.into_iter().flat_map(|mut c| match c["body"].take() {
+        let content = json::parse(&content)?;
+        let comments = match content {
+            json::JsonValue::Array(cs) => Ok(Box::new(cs.into_iter().flat_map(|mut c| {
+                match c["body"].take() {
                     json::JsonValue::String(s) => Some(s),
                     _ => None,
-                }))
-            }
+                }
+            }))),
             _ => Err(Error::JsonParseErr),
-        }
+        }?;
+        Ok(Page {
+            comments: comments,
+            next: next_link(res.headers()).map(|s| s.to_owned()),
+        })
     }
+}
+
+pub fn fetch<'a>(repo: &str, issue: &str, key: Option<&'a str>) -> Result<Comments<'a>, Error> {
+    let client = Client::new();
+    let url = format!("{}/repos/{}/issues/{}/comments", API_ROOT, repo, issue);
+    let page = fetch_page(&client, &url, key)?;
+    Ok(Comments {
+        client: client,
+        key: key,
+        comments: page.comments,
+        next: page.next,
+    })
 }
